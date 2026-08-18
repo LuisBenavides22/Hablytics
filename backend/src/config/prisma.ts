9781e1207ab88fadcd912bfe4a0bf9c8a04f6574
Luis Benavides <luisbenavidese99@gmail.com>
2026-08-18 12:22:54 -0500

    Update

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -10,13 +10,12 @@ if (!connectionString) {
     throw new Error('DATABASE_URL is missing in .env file');
 }
 
-// 1. Initialize a PostgreSQL connection pool using the 'pg' library
 const pool = new Pool({ connectionString });
 
-// 2. Wrap the pool in Prisma's official PostgreSQL adapter
+
 const adapter = new PrismaPg(pool);
 
-// 3. Pass the adapter into the Prisma Client (This is the Prisma 7 way)
+
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
